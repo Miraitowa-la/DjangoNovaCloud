bd63f7ae9b9ca2c0b5fd888e586a58963106f58f
Miraitowa <2056978412@qq.com>
2025-05-20 01:17:51 +0800

    部分四：操作审计日志的"操作类型"优化

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -47,9 +47,47 @@ class AuditLog(models.Model):
     ACTION_USER_LOGIN = 'user_login'
     ACTION_USER_LOGOUT = 'user_logout'
     ACTION_USER_LOGIN_FAILED = 'user_login_failed'
+    
+    # 项目相关操作
     ACTION_PROJECT_CREATE = 'project_created'
     ACTION_PROJECT_UPDATE = 'project_updated'
     ACTION_PROJECT_DELETE = 'project_deleted'
+    
+    # 设备相关操作
+    ACTION_DEVICE_CREATE = 'device_created'
+    ACTION_DEVICE_UPDATE = 'device_updated'
+    ACTION_DEVICE_DELETE = 'device_deleted'
+    ACTION_DEVICE_KEY_RESET = 'device_key_reset'
+    ACTION_DEVICE_COMMAND = 'device_command_sent'
+    
+    # 传感器相关操作
+    ACTION_SENSOR_CREATE = 'sensor_created'
+    ACTION_SENSOR_UPDATE = 'sensor_updated'
+    ACTION_SENSOR_DELETE = 'sensor_deleted'
+    
+    # 执行器相关操作
+    ACTION_ACTUATOR_CREATE = 'actuator_created'
+    ACTION_ACTUATOR_UPDATE = 'actuator_updated'
+    ACTION_ACTUATOR_DELETE = 'actuator_deleted'
+    ACTION_ACTUATOR_CONTROL = 'actuator_controlled'
+    
+    # 策略相关操作
+    ACTION_STRATEGY_CREATE = 'strategy_created'
+    ACTION_STRATEGY_UPDATE = 'strategy_updated'
+    ACTION_STRATEGY_DELETE = 'strategy_deleted'
+    ACTION_STRATEGY_ENABLE = 'strategy_enabled'
+    ACTION_STRATEGY_DISABLE = 'strategy_disabled'
+    ACTION_STRATEGY_EXECUTE = 'strategy_executed'
+    
+    # 系统配置相关操作
+    ACTION_SYSTEM_CONFIG = 'system_configured'
+    ACTION_BACKUP_CREATE = 'backup_created'
+    ACTION_BACKUP_RESTORE = 'backup_restored'
+    
+    # 其他操作
+    ACTION_API_ACCESS = 'api_accessed'
+    ACTION_FILE_UPLOAD = 'file_uploaded'
+    ACTION_FILE_DOWNLOAD = 'file_downloaded'
     
     ACTION_CHOICES = [
         (ACTION_USER_CREATE, '创建用户'),
@@ -62,9 +100,47 @@ class AuditLog(models.Model):
         (ACTION_USER_LOGIN, '用户登录'),
         (ACTION_USER_LOGOUT, '用户登出'),
         (ACTION_USER_LOGIN_FAILED, '登录失败'),
+        
+        # 项目相关操作
         (ACTION_PROJECT_CREATE, '创建项目'),
         (ACTION_PROJECT_UPDATE, '更新项目'),
         (ACTION_PROJECT_DELETE, '删除项目'),
+        
+        # 设备相关操作
+        (ACTION_DEVICE_CREATE, '创建设备'),
+        (ACTION_DEVICE_UPDATE, '更新设备'),
+        (ACTION_DEVICE_DELETE, '删除设备'),
+        (ACTION_DEVICE_KEY_RESET, '重置设备密钥'),
+        (ACTION_DEVICE_COMMAND, '发送设备命令'),
+        
+        # 传感器相关操作
+        (ACTION_SENSOR_CREATE, '创建传感器'),
+        (ACTION_SENSOR_UPDATE, '更新传感器'),
+        (ACTION_SENSOR_DELETE, '删除传感器'),
+        
+        # 执行器相关操作
+        (ACTION_ACTUATOR_CREATE, '创建执行器'),
+        (ACTION_ACTUATOR_UPDATE, '更新执行器'),
+        (ACTION_ACTUATOR_DELETE, '删除执行器'),
+        (ACTION_ACTUATOR_CONTROL, '控制执行器'),
+        
+        # 策略相关操作
+        (ACTION_STRATEGY_CREATE, '创建策略'),
+        (ACTION_STRATEGY_UPDATE, '更新策略'),
+        (ACTION_STRATEGY_DELETE, '删除策略'),
+        (ACTION_STRATEGY_ENABLE, '启用策略'),
+        (ACTION_STRATEGY_DISABLE, '禁用策略'),
+        (ACTION_STRATEGY_EXECUTE, '执行策略'),
+        
+        # 系统配置相关操作
+        (ACTION_SYSTEM_CONFIG, '系统配置'),
+        (ACTION_BACKUP_CREATE, '创建备份'),
+        (ACTION_BACKUP_RESTORE, '恢复备份'),
+        
+        # 其他操作
+        (ACTION_API_ACCESS, 'API访问'),
+        (ACTION_FILE_UPLOAD, '文件上传'),
+        (ACTION_FILE_DOWNLOAD, '文件下载'),
     ]
     
     user = models.ForeignKey(
